9ed17ef63fc5eb3d9278fe5d9fc934bdc3c56d84
RandyCheng <linusche@gmail.com>
2017-11-29 11:36:49 +0800

    加入專案檔案。

---
+++ MyThread/FrmThread.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lab_06
+{
+    public partial class FrmThread : Form
+    {
+        public FrmThread()
+        {
+            InitializeComponent();
+            
+            
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            long sum =LongTask();
+            MessageBox.Show("sum= "+sum);
+
+        }
+        long LongTask()//單一執行緒會拖住其他功能執行包括UI
+        {
+            long sum = 0;
+            for(long i = 1; i <= int.MaxValue; i++)
+            {
+                sum += 1;
+            }
+            return sum;
+        }
+
+        private void FrmThread_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            this.Text = DateTime.Now.ToLongTimeString();
+        }
+    }
+}
